cede6fedc09bb558f32325339c9fbb96ce78c65b
Ariana Freitag <freitagariana@gmail.com>
2019-04-16 22:53:03 -0400

    working on adding api calls

--- src/api/api.js
+++ src/api/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: `http://localhost:9999/`
-  baseURL: `http://127.0.0.1/api`
+  baseURL: `http://192.168.1.90:9999`
 });
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,26 +1,26 @@
-// import { Layout, Form, Icon, Input, Button, Checkbox } from "antd";
-// import React, { Component } from "react";
-// import "antd/dist/antd.css";
-// import Header from "../../components/header/header";
-// import Footer from "../../components/footer/footer";
-// import { connect } from "react-redux";
-//
-// class Home extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { redirectToReferrer: false };
-//   }
-//
-//   render() {
-//     return (
-//       <Layout className="layout">
-//         <Header />
-//         <Footer />
-//       </Layout>
-//     );
-//   }
-// }
-//
-// export default connect(state => ({
-//   isLoggedIn: state.Auth.idToken !== null
-// }))(Home);
+import { Layout, Form, Icon, Input, Button, Checkbox } from "antd";
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import { connect } from "react-redux";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { redirectToReferrer: false };
+  }
+
+  render() {
+    return (
+      <Layout className="layout">
+        <Header />
+        <Footer />
+      </Layout>
+    );
+  }
+}
+
+export default connect(state => ({
+  isLoggedIn: state.Auth.idToken !== null
+}))(Home);
