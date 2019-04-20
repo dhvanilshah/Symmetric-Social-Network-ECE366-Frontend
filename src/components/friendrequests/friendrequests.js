3d4a5cf5ee34058eb7ce5adf53db34fe936a8b2c
Dhvanil Shah <dhv_19@yahoo.com>
2019-04-19 22:45:33 -0400

    added add friend UI

---
+++ src/components/friendrequests/friendrequests.js
@@ -0,0 +1,53 @@
+import { Layout, Popover, List, Avatar, Icon, Button } from "antd";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import ProfileCircle from "../uielements/profileCircle";
+import Complete from "../searchbar/searchbar";
+
+const { Header } = Layout;
+
+const data = [
+  {
+    name: "Ant Design Title 1",
+    username: "Username1"
+  },
+  {
+    name: "Ant Design Title 2",
+    username: "Username2"
+  },
+  {
+    name: "Ant Design Title 3",
+    username: "Username3"
+  },
+  {
+    name: "Ant Design Title 4",
+    username: "Username4"
+  }
+];
+
+const content = (
+  <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={item => (
+      <List.Item actions={[<Button>Accept</Button>, <a>Decline</a>]}>
+        <List.Item.Meta title={item.name} description={item.username} />
+      </List.Item>
+    )}
+  />
+);
+
+class FriendRequests extends Component {
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <Popover content={content} title="Friend Requests" trigger="click">
+        <Icon type="usergroup-add" />
+      </Popover>
+    );
+  }
+}
+
+export default connect(state => ({
+  isLoggedIn: state.Auth.idToken !== null
+}))(FriendRequests);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,19 @@
-import { Layout } from "antd";
+import { Layout, Popover, Button, Icon } from "antd";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import ProfileCircle from "../uielements/profileCircle";
 import "./header.css";
 import Complete from "../searchbar/searchbar";
+import FriendRequests from "../friendrequests/friendrequests";
 
 const { Header } = Layout;
+
+const content = (
+  <div>
+    <p>Content</p>
+    <p>Content</p>
+  </div>
+);
 
 class HeaderBar extends Component {
   render() {
@@ -21,7 +29,12 @@ class HeaderBar extends Component {
       >
         <div className="logo" />
         {isLoggedIn ? <Complete /> : null}
-        {isLoggedIn ? <ProfileCircle /> : null}
+        {isLoggedIn ? (
+          <div>
+            <FriendRequests />
+            <ProfileCircle />
+          </div>
+        ) : null}
       </Header>
     );
   }
