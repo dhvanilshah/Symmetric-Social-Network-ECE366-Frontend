7af6ae62ec15b28da629182a1186ea8021bae419
Dhvanil Shah <dhv_19@yahoo.com>
2019-04-28 23:00:12 -0400

    added friends list

--- src/components/friendslist/friendslist.js
+++ src/components/friendslist/friendslist.js
@@ -1,54 +1,36 @@
 import { Layout, Popover, List, Avatar, Icon, Button, message } from "antd";
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import API from "../../api/api";
 
-const data = [
-  {
-    title: "User1 Name"
-  },
-  {
-    title: "User2 Name"
-  },
-  {
-    title: "User3 Name"
-  },
-  {
-    title: "User4 Name"
-  },
-  {
-    title: "User1 Name"
-  },
-  {
-    title: "User2 Name"
-  },
-  {
-    title: "User3 Name"
-  },
-  {
-    title: "User4 Name"
-  },
-  {
-    title: "User1 Name"
-  },
-  {
-    title: "User2 Name"
-  },
-  {
-    title: "User3 Name"
-  },
-  {
-    title: "User4 Name"
-  }
-];
+const data2 = [{ name: "Dhvanil", username: "dhvanilshah" }];
 
 class FriendList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+    this.getFriends = this.getFriends.bind(this);
+  }
+
+  async getFriends() {
+    console.log("called");
+    const data = await API.get("getFriends");
+    this.setState({ data: data.data.payload.value });
+    console.log(data.data.payload.value);
+  }
+
+  componentWillMount() {
+    this.getFriends();
+  }
+
   render() {
     const { isLoggedIn } = this.props;
+    const { data } = this.state;
     return (
       <List
         itemLayout="horizontal"
         header={<h1>Friends List</h1>}
-        dataSource={data}
+        dataSource={data.length > 0 ? data : []}
         renderItem={item => (
           <List.Item
             onClick={() => console.log("Do Something Here")}
@@ -59,8 +41,8 @@ class FriendList extends Component {
             ]}
           >
             <List.Item.Meta
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="username"
+              title={<a href="https://ant.design">{item.name}</a>}
+              description={item.username}
             />
           </List.Item>
         )}
